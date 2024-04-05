fc500b8b3fcca9183ccf655dfcbf8c3341930375
utsavbhattarai007 <ninja08469@gmail.com>
2024-04-05 09:06:22 +0545

    Theme setup with Nav bar styling

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import Nav from "@/components/ui/nav"
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight:["400","500","600"],subsets: ["latin"], display: "swap",style: "normal",variable:"--font-poppins" });
 
 export const metadata: Metadata = {
   title: "Ctodo",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.variable}>
+        <Nav/>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ web/components/ui/nav.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <>
+    <div className="w-full flex justify-center items-center p-4">
+        <div className="w-4/5 rounded-lg h-14 bg-forebackground border-border border-[2px] flex items-center justify-between px-4 py-2">
+            <div className="h-full flex justify-center items-center">
+                <p className="text-3xl text-text font-semibold font-pops">
+                    C
+                    <span className="text-primary">
+                     Todo
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default nav
