19345b5ff62c683a99912e72aa797ef8cb4cb98e
utsavbhattarai007 <ninja08469@gmail.com>
2024-04-07 21:56:22 +0545

    Configured theme

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,9 +1,16 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import Nav from "@/components/ui/nav"
+import Nav from "@/components/ui/nav";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
-const poppins = Poppins({ weight:["400","500","600"],subsets: ["latin"], display: "swap",style: "normal",variable:"--font-poppins" });
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+  subsets: ["latin"],
+  display: "swap",
+  style: "normal",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Ctodo",
@@ -16,10 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={poppins.variable}>
-        <Nav/>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Nav />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
